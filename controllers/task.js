0df0aaafe34e1fe8f2697c98c0dc53ca4b9d3b37
yadav-ankita <ankitakumari16006@gmail.com>
2025-05-19 23:13:46 +0530

    using Async Wrappers for try/catch

--- controllers/task.js
+++ controllers/task.js
@@ -5,8 +5,7 @@ const allTask = asyncWrapper(async (req, res) => {
     res.status(200).json({ tasks: task });
 })
 const createTask = asyncWrapper(async (req, res) => {
-
-    const task = await Task.create(req.body);
+     const task = await Task.create(req.body);
     res.status(201).json(task);
 })
 const getTask = asyncWrapper(async (req, res) => {
